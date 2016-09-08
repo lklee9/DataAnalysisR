943102d47a98183cc30c16b498fec6326688c888
Lee Loong Kuan <kennylklee48@gmail.com>
2016-09-08 13:11:07 +1000

    Added prettier visualisation and visualisation running

--- main.R
+++ main.R
@@ -3,8 +3,16 @@ source("visualisation.R")
 source("result_table.R")
 
 
-file_path = "../data_out/martvard/airlines_2-attributes_prior.csv"
-resultTable <- ResultTable(file_path)
-attributes.all <- ExtractAttributes(resultTable)
-distanceMatrix <- DistanceMatrix(resultTable)
-MDS(distanceMatrix, attributes.all)
+#file_path = "../data_out/martvard/airlines_4-attributes_prior.csv"
+file_path = "../data_out/martvard/20130622_20131129_4-attributes_prior.csv"
+#file_path = "../data_out/martvard/elecNormNew_4-attributes_prior.csv"
+#file_path = "../data_out/martvard/sensor_4-attributes_prior.csv"
+
+runVisualisation <- function(file_path) {
+  resultTable <- ResultTable(file_path)
+  attributes.all <- ExtractAttributes(resultTable)
+  distanceMatrix <- DistanceMatrix(resultTable)
+  MDS(distanceMatrix, attributes.all)
+}
+
+runVisualisation(file_path)
