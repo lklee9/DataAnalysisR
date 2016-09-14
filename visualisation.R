b339c1b2e7dc9ffbbd73dcbabec671874e7bff92
Lee Loong Kuan <kennylklee48@gmail.com>
2016-09-14 14:34:29 +0000

    Tweak discrete histogram and bins for continuous.

--- visualisation.R
+++ visualisation.R
@@ -43,13 +43,13 @@ SingleHistogram <- function(data1, data2, column.index) {
     data.all <- rbind(data.all.1, data.all.2)
     plot <- NA
     if (is.factor(data.all[, 1])) {
-      plot <- ggplot(data.all, aes(value, fill = type)) + geom_bar(width = 1, alpha = 0.5)
+      plot <- ggplot(data.all, aes(value, fill = type)) + geom_bar(width = 1, alpha = 0.5, position = 'dodge')
       plot <- plot + labs(xlab(column.name))
     }
     else {
       if ((max(data.all[,1]) - min(data.all[,1])) > 0) {
         plot <- ggplot(data.all, aes(value, fill = type)) + 
-          geom_histogram(alpha = 0.5, position = "identity", binwidth = ceiling((max(data.all[,1]) - min(data.all[,1]))/100))
+          geom_histogram(alpha = 0.5, position = "identity", binwidth = ((max(data.all[,1]) - min(data.all[,1]))/50))
         plot <- plot + labs(xlab(column.name))
       }
     }
